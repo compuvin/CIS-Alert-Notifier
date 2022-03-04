ff59eee96687b73be71fb5c49386004b1381a210
compuvin <compuvin@users.noreply.github.com>
2022-03-04 09:16:04 -0500

    Initial commit

---
+++ CIS-Alert-Notifier.ps1
@@ -0,0 +1,63 @@
+﻿##################################
+#Edit these values as you see fit
+$DBLocation = "localhost"
+$DBSchema = "CCPD"
+$DBUser = "user"
+$DBPass = "password"
+
+$MailFromEmail = "email@domain.com"
+$MailServer = "mail.domain.com"
+##################################
+
+#Get last read record from .dat file
+$LastReadRec = 0
+if (Test-Path -Path LastReadRec.dat -PathType Leaf)
+{
+    $LastReadRec = Get-Content LastReadRec.dat
+} else {
+    $LastReadRec | Set-Content LastReadRec.dat
+}
+
+#Open SQL Connection
+$myconnection = New-Object System.Data.SqlClient.SqlConnection
+$myconnection.ConnectionString = "Database=" + $DBSchema + ";server=" + $DBLocation + ";Persist Security Info=false;user id=" + $DBUser + ";pwd=" + $DBPass + ";"
+$myconnection.Open()
+$command = $myconnection.CreateCommand()
+
+#Get user list
+#$command.CommandText = "select id, first_name, email from dbo.ccpd_user";
+#$Usertable = New-Object System.Data.DataTable
+#$UserTable.Load($command.ExecuteReader())
+
+#Get unread alerts
+$command.CommandText = "select AlertTb.id, AlertTb.title, UserTb.first_name, UserTb.email from dbo.alert_instance as AlertTb inner join dbo.ccpd_user as UserTb on AlertTb.recipient_id = UserTb.id where AlertTb.is_read=0 and AlertTb.id>" + $LastReadRec + " order by AlertTb.id;"
+$AlertTable = New-Object System.Data.DataTable
+$AlertTable.Load($command.ExecuteReader())
+
+#$AlertTable | Format-Table
+
+#Loop through new alerts and send emails
+foreach ($Row in $AlertTable)
+{
+    $command.CommandText = "select element_name, element_value from dbo.alert_instance_data where alert_instance_id=" + $Row.id + ";";
+    $reader = $command.ExecuteReader()
+
+    $outputl = "<p>" + $Row.first_name + ",</p><p>You have a new alert in the CIS-CAT Pro Dashboard.</p>"
+    $outputl = $outputl + "<p>"
+
+    while ($reader.Read()) {
+        $outputl = $outputl + $Reader["element_name"].ToString() + " : " + $Reader["element_value"].ToString() + "<br>"
+    }
+    $reader.close()
+    $outputl = $outputl + "</p>"
+
+    #send the email
+    if ($Row.email -notcontains "@ciscatpro.org"){
+        Send-MailMessage -From $MailFromEmail -To $Row.email -SmtpServer $MailServer -Subject ("CCPD Alert: " + $Row.title) -Body $outputl -BodyAsHtml
+    }
+}
+
+if ($AlertTable.Rows.Count -gt 0) {$AlertTable.Rows[$AlertTable.Rows.Count-1].id | Set-Content LastReadRec.dat}
+
+#Close SQL Connection
+$myconnection.Close()
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # CIS-Alert-Notifier
 Provides email notification functionality for the CIS-CAT Pro Dashboard
+
+This Powershell script interfaces with the backend database for your CIS-CAT Pro Dashboard installation. It profides much needed notification functionality for alerts within the dashboard.
+<br><br>
+To set up, simply copy the Powershell script to a new folder on your database server. Edit the lines indicated near the top of the script. Save it and then use Tash Scheduler to schedule it to run at intervals (e.g. every 15 minutes).
+<br><br>
+For simplicity, this script was designed for use with an in-house email server that allows local SMPT connections. However, it can easily be edited to support modern secure email servers.
+<br><br>
+<a href="https://www.codefactor.io/repository/github/compuvin/cis-alert-notifier"><img src="https://www.codefactor.io/repository/github/compuvin/cis-alert-notifier/badge" alt="CodeFactor" /></a>
